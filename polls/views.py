f305253b866f9543f94eba2622b968f1173cbbda
Antonia Blair <antoniablair@gmail.com>
2015-03-29 00:59:12 -0400

    Updates to use Generic views

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.template import RequestContext
+from django.views import generic
 
 from polls.models import Choice, Question
 
@@ -9,70 +9,46 @@ from polls.models import Choice, Question
 # a template name as its second argument and a dictionary as its optional third argument. 
 # It returns an HttpResponse object of the given template rendered with the given context.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# Using two generic views, ListView and DetailView
+# Each needs to know which model it will be acting on with the model attribute
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# The DetailView generic view expects the primary key value captured from the URL 
+# to be called "pk"
+
+# By default, List view uses the template  <app name>/<model name>_list.html 
+# aka: (polls/index.html)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+   		"""Return the last five published questions."""
+   		return Question.objects.order_by('-pub_date')[:5]
+
+
+# By default, uses the template <app name>/<model name>_detail.html 
+# aka: polls/question_detail.html
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
 		p = get_object_or_404(Question, pk=question_id)
 		try:
-				# request.POST is a dictionary-like object that lets you access submitted
-				# data by key name. Here, request.POST['choice'] returns the ID of the 
-				# selected choice as a string -- request.POST variables are always strings
-				selected_choice = p.choice_set.get(pk=request.POST['choice'])
-
-				# Will raise a key error if choice was not provided in POST data and
-				# redisplay the question voting form with an error message 
+			selected_choice = p.choice_set.get(pk=request.POST['choice'])
 		except (KeyError, Choice.DoesNotExist):
-				return render(request, 'polls/detail.html', {
-						'question': p,
-						'error_message': "You didn't select a choice!",
-					})
+			return render(request, 'polls/detail.html', {
+				'question': p,
+				'error_message': "You didn't select a choice.",
+			})
 		else:
-				selected_choice.votes += 1
-				selected_choice.save()
-				# Always return an HttpResponseRedirect after successfully dealing
-		    # with POST data. This prevents data from being posted twice if a
-		    # user hits the Back button.
-
-				# HttpResponseRedirect takes a single argument: 
-				# the URL to which the user will be redirected
-				return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-# ALTERNATIVES
-
-# How to do it if you don't import Render:
-# from django.template import RequestContext, loader
-
-# What to do below if you don't import the Render function:
-# Load the template polls/index.html and passes it a context
-# (The context is a dictionary mapping template variable names to Python objects) See below
-		
-# def index(request):
-
-# 		latest_question_list = Question.objects.order_by('-pub_date')[:5]
-# 		template = loader.get_template('polls/index.html')
-# 		context = RequestContext(request, {
-# 		    'latest_question_list': latest_question_list,
-# 		})
-# 		return HttpResponse(template.render(context))
-
-#  --------------
-
-# Catch with a 404 if does not exist. This works too-- 
-
-# def detail(request, question_id):
-# try: 
-# 		question = Question.objects.get(pk=question_id)
-# except Question.DoesNotExist:
-# 		raise Http404("Question does not exist, sorry!")
-# return render(request, 'polls/detail.html', {'question': question})
+			selected_choice.votes += 1
+			selected_choice.save()
+			return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
--- polltutorial/urls.py
+++ polltutorial/urls.py
@@ -2,13 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'polltutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-		# url(r'^detail/', include(detail.site.urls)),
-		# url(r'^results/', include(results.site.urls)),
-		# url(r'^vote/', include(vote.site.urls)),
 )
